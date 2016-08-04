895f5c1eb12833dc5190f260dfd0e8c1f69d3854
davidchampredon <david.champredon@gmail.com>
2016-08-04 15:49:34 -0400

    new functions for R wrap

--- Rwrap.cpp
+++ Rwrap.cpp
@@ -43,34 +43,34 @@ List to_list(const dcDataFrame & df,
 
 
 void set_parameter(modelParam &MP, string prm_name, string prm_type, List params){
-	
-	bool found = false;
-	
-	if (prm_type=="double") {
-		double val = params[prm_name];
-		MP.add_prm_double(prm_name, val);
-		found = true;
-	}
-	
-	else if (prm_type=="bool") {
-		bool val = params[prm_name];
-		MP.add_prm_bool(prm_name, val);
-		found = true;
-	}
-	
-	else if (prm_type=="string") {
-		string val = params[prm_name];
-		MP.add_prm_string(prm_name, val);
-		found = true;
-	}
-	
-	else if (prm_type=="uint") {
-		uint val = params[prm_name];
-		MP.add_prm_uint(prm_name, val);
-		found = true;
-	}
-	if(!found)
-		cerr << "Parameter type " << prm_type << " not found for parameter "<<prm_name <<endl;
+    
+    bool found = false;
+    
+    if (prm_type=="double") {
+        double val = params[prm_name];
+        MP.add_prm_double(prm_name, val);
+        found = true;
+    }
+    
+    else if (prm_type=="bool") {
+        bool val = params[prm_name];
+        MP.add_prm_bool(prm_name, val);
+        found = true;
+    }
+    
+    else if (prm_type=="string") {
+        string val = params[prm_name];
+        MP.add_prm_string(prm_name, val);
+        found = true;
+    }
+    
+    else if (prm_type=="uint") {
+        uint val = params[prm_name];
+        MP.add_prm_uint(prm_name, val);
+        found = true;
+    }
+    if(!found)
+        cerr << "Parameter type " << prm_type << " not found for parameter "<<prm_name <<endl;
 }
 
 
@@ -83,96 +83,207 @@ void set_parameter(modelParam &MP, string prm_name, string prm_type, List params
 
 
 // [[Rcpp::export]]
-List naf_run(List params, List simulParams){
-	/// Main test
-	
-	vector<unsigned int> res;
-	
-	// Model parameters:
-	modelParam MP;
-	
-	set_parameter(MP, "debug_mode", "bool", params);
-	
-	set_parameter(MP, "homogeneous_contact", "bool", params);
-	
-	set_parameter(MP, "dol_distrib", "string", params);
-	set_parameter(MP, "doi_distrib", "string", params);
-	set_parameter(MP, "doh_distrib", "string", params);
-	
-	set_parameter(MP, "dol_mean", "double", params);
-	set_parameter(MP, "doi_mean", "double", params);
-	set_parameter(MP, "doh_mean", "double", params);
+List naf_run(List params,
+             List simulParams,
+             List interventionParams,
+             List worldParams,
+             List scheduleParams){
+    /// Main test
+    
+    vector<unsigned int> res;
+    
+    // ==== Model parameters ====
+    
+    modelParam MP;
+    
+    set_parameter(MP, "debug_mode", "bool", params);
+    
+    set_parameter(MP, "homogeneous_contact", "bool", params);
+    
+    set_parameter(MP, "dol_distrib", "string", params);
+    set_parameter(MP, "doi_distrib", "string", params);
+    set_parameter(MP, "doh_distrib", "string", params);
+    
+    set_parameter(MP, "dol_mean", "double", params);
+    set_parameter(MP, "doi_mean", "double", params);
+    set_parameter(MP, "doh_mean", "double", params);
+    
+    set_parameter(MP, "proba_move", "double", params);
+    set_parameter(MP, "contact_rate", "double", params);
+    
+    set_parameter(MP, "asymptom_infectiousness_ratio", "double", params);
+    
+    set_parameter(MP, "treat_doi_reduc", "double", params);
+    set_parameter(MP, "treat_reduc_infect_mean", "double", params);
+    
+    set_parameter(MP, "vax_imm_incr", "double", params);
+    set_parameter(MP, "vax_frail_incr", "double", params);
+    set_parameter(MP, "vax_lag_full_efficacy", "double", params);
+    
+    set_parameter(MP, "proba_death_prm_1", "double", params);
+    set_parameter(MP, "proba_death_prm_2", "double", params);
+    set_parameter(MP, "proba_death_prm_3", "double", params);
+    
+    
+    // ==== Simulation parameters ====
+    
+    unsigned int rnd_seed   = simulParams["rnd_seed"];
+    unsigned int i0         = simulParams["initial_latent"];
+    double horizon          = simulParams["horizon"];
+    unsigned int nt         = simulParams["nt"];
+    unsigned int popexport  = simulParams["popexport"];
+    
+    
+    // === Interventions ===
+    
+    string	interv_name     = interventionParams["interv_name"];
+    string	interv_type     = interventionParams["interv_type"];
+    string	interv_target   = interventionParams["interv_target"];
+    double	interv_start    = interventionParams["interv_start"];
+    double	interv_end      = interventionParams["interv_end"];
+    double	interv_cvg_rate = interventionParams["interv_cvg_rate"];
+    double	interv_cvg_max_prop = interventionParams["interv_cvg_max_prop"];
+    
+    intervention interv(interv_type,
+                        interv_target,
+                        interv_name,
+                        interv_start,
+                        interv_end,
+                        interv_cvg_rate,
+                        interv_cvg_max_prop);
+    
+    
+    // === World population parameters ===
+    
+    vector<ID> id_au = worldParams["id_au"];
+    vector<string> name_au = worldParams["name_au"];
+    ID id_region = worldParams["id_region"];
+    string regionName = worldParams["regionName"];
+    
+    
+    vector<uint> pr_age_hh_00_val = worldParams["pr_age_hh_00_val"];
+    vector<uint> pr_age_hh_10_val = worldParams["pr_age_hh_10_val"];
+    vector<uint> pr_age_hh_11_val = worldParams["pr_age_hh_11_val"];
+    vector<uint> pr_age_hh_20_val = worldParams["pr_age_hh_20_val"];
+    vector<uint> pr_age_hh_21_val = worldParams["pr_age_hh_21_val"];
+    vector<uint> pr_age_hh_22_val = worldParams["pr_age_hh_22_val"];
+    
+    vector<double> pr_age_hh_00_proba = worldParams["pr_age_hh_00_proba"];
+    vector<double> pr_age_hh_10_proba = worldParams["pr_age_hh_10_proba"];
+    vector<double> pr_age_hh_11_proba = worldParams["pr_age_hh_11_proba"];
+    vector<double> pr_age_hh_20_proba = worldParams["pr_age_hh_20_proba"];
+    vector<double> pr_age_hh_21_proba = worldParams["pr_age_hh_21_proba"];
+    vector<double> pr_age_hh_22_proba = worldParams["pr_age_hh_22_proba"];
+    
+    
+    vector<uint> hh_size			= worldParams["hh_size"];
+    vector<double> hh_size_proba	= worldParams["hh_size_proba"];
+    vector<uint> wrk_size			= worldParams["wrk_size"];
+    vector<double> wrk_size_proba	= worldParams["wrk_size_proba"];
+    vector<uint> pubt_size			= worldParams["pubt_size"];
+    vector<double> pubt_size_proba  = worldParams["pubt_size_proba"];
+    vector<uint> school_size		= worldParams["school_size"];
+    vector<double> school_size_proba= worldParams["school_size_proba"];
+    vector<uint> hosp_size			= worldParams["hosp_size"];
+    vector<double> hosp_size_proba	= worldParams["hosp_size_proba"];
+    vector<uint> other_size			= worldParams["other_size"];
+    vector<double> other_size_proba = worldParams["other_size_proba"];
+    
+    vector<uint> n_hh		= worldParams["n_hh"];
+    vector<uint> n_wrk		= worldParams["n_wrk"];
+    vector<uint> n_pubt		= worldParams["n_pubt"];
+    vector<uint> n_school	= worldParams["n_school"];
+    vector<uint> n_hosp		= worldParams["n_hosp"];
+    vector<uint> n_other	= worldParams["n_other"];
+    
+    vector<areaUnit> auvec = create_area_unit(id_au, name_au, id_region, regionName);
+    
+    // Social place sizes distributions:
+    discrete_prob_dist<uint> D_size_hh(hh_size, hh_size_proba);
+    discrete_prob_dist<uint> D_size_wrk(wrk_size, wrk_size_proba);
+    discrete_prob_dist<uint> D_size_pubt(pubt_size, pubt_size_proba);
+    discrete_prob_dist<uint> D_size_school(school_size, school_size_proba);
+    discrete_prob_dist<uint> D_size_hosp(hosp_size, hosp_size_proba);
+    discrete_prob_dist<uint> D_size_other(other_size, other_size_proba);
+    
 
-	set_parameter(MP, "proba_move", "double", params);
-	set_parameter(MP, "contact_rate", "double", params);
-
-	set_parameter(MP, "asymptom_infectiousness_ratio", "double", params);
-	
-	set_parameter(MP, "treat_doi_reduc", "double", params);
-	set_parameter(MP, "treat_reduc_infect_mean", "double", params);
-	
-	set_parameter(MP, "vax_imm_incr", "double", params);
-	set_parameter(MP, "vax_frail_incr", "double", params);
-	set_parameter(MP, "vax_lag_full_efficacy", "double", params);
-	
-	set_parameter(MP, "proba_death_prm_1", "double", params);
-	set_parameter(MP, "proba_death_prm_2", "double", params);
-	set_parameter(MP, "proba_death_prm_3", "double", params);
-
-	
-	// Simulation parameters:
-	
-	unsigned int rnd_seed   = simulParams["rnd_seed"];
-	unsigned int n_indiv    = simulParams["n_indiv"];
-	unsigned int i0         = simulParams["initial_latent"];
-	double horizon          = simulParams["horizon"];
-	unsigned int nt         = simulParams["nt"];
-	unsigned int popexport  = simulParams["popexport"];
-	
-	string	interv_name     = simulParams["interv_name"];
-	string	interv_type     = simulParams["interv_type"];
-	string	interv_target   = simulParams["interv_target"];
-	double	interv_start    = simulParams["interv_start"];
-	double	interv_end      = simulParams["interv_end"];
-	double	interv_cvg_rate = simulParams["interv_cvg_rate"];
-	double	interv_cvg_max_prop = simulParams["interv_cvg_max_prop"];
-
-	
-	
-	cout << "DEBUG: popexport is "<< popexport <<endl;
-	
-	
-	// Define intervention
-	intervention interv_test(interv_type,
-							 interv_target,
-							 interv_name,
-							 interv_start,
-							 interv_end,
-							 interv_cvg_rate,
-							 interv_cvg_max_prop);
-	
-	
-	// Call C++ function
-	
-	_RANDOM_GENERATOR.seed(rnd_seed);
-	
-	Simulation sim = test_naf(MP,
-							  horizon,
-							  n_indiv,
-							  i0,
-							  interv_test);
-	
-	// Retrieve all results from simulation:
-	// populations:
-	dcDataFrame pop_final = sim.get_world()[popexport].export_dcDataFrame();
-	// epidemic time series
-	dcDataFrame ts = sim.timeseries();
-	dcDataFrame ts_census = sim.get_ts_census_by_SP();
-	
-	// Return R-formatted result:
-	return List::create(Named("population_final") = to_list(pop_final,false),
-						Named("time_series") = to_list(ts, false),
-						Named("time_series_census") = to_list(ts_census, false));
+    // within households age conditional distributions:
+    vector< vector<discrete_prob_dist<uint> > > pr_age_hh;
+    
+    discrete_prob_dist<uint> pr_age_hh_00(pr_age_hh_00_val, pr_age_hh_00_proba);
+    discrete_prob_dist<uint> pr_age_hh_10(pr_age_hh_10_val, pr_age_hh_10_proba);
+    discrete_prob_dist<uint> pr_age_hh_11(pr_age_hh_11_val, pr_age_hh_11_proba);
+    discrete_prob_dist<uint> pr_age_hh_20(pr_age_hh_20_val, pr_age_hh_20_proba);
+    discrete_prob_dist<uint> pr_age_hh_21(pr_age_hh_21_val, pr_age_hh_21_proba);
+    discrete_prob_dist<uint> pr_age_hh_22(pr_age_hh_22_val, pr_age_hh_22_proba);
+    
+    vector<discrete_prob_dist<uint> > tmp;
+    tmp.push_back(pr_age_hh_00);
+    pr_age_hh.push_back(tmp);
+    tmp.clear();
+    tmp = {pr_age_hh_10,pr_age_hh_11};
+    pr_age_hh.push_back(tmp);
+    tmp.clear();
+    tmp = {pr_age_hh_20,pr_age_hh_21,pr_age_hh_22};
+    pr_age_hh.push_back(tmp);
+    
+    
+    
+    // === Schedules definition ===
+    
+    // Define the time slices
+    // must be same for all schedules and must sum up to 1.0
+    vector<double> timeslice = scheduleParams["timeslice"];
+    
+    // type of schedules:
+    vector<SPtype> worker_sed   {SP_pubTransp, SP_workplace, SP_workplace, SP_pubTransp, SP_other, SP_household};
+    vector<SPtype> student      {SP_pubTransp, SP_school,    SP_school,    SP_pubTransp, SP_other, SP_household};
+    vector<SPtype> unemployed   {SP_household, SP_other,     SP_other,     SP_other,     SP_other, SP_household};
+    
+    schedule sched_worker_sed(worker_sed, timeslice, "worker_sed");
+    schedule sched_student(student, timeslice, "student");
+    
+    // Schedules used in the simulation:
+    vector<schedule> sched {
+        sched_worker_sed,
+        sched_student
+    };
+    
+    // === Call C++ function ===
+    
+    _RANDOM_GENERATOR.seed(rnd_seed);
+    
+    Simulation sim = run_test(auvec,
+                              D_size_hh,
+                              D_size_wrk,
+                              D_size_pubt,
+                              D_size_school,
+                              D_size_hosp,
+                              D_size_other,
+                              pr_age_hh,
+                              n_hh ,
+                              n_wrk,
+                              n_pubt ,
+                              n_school,
+                              n_hosp,
+                              n_other,
+                              sched ,
+                              MP,
+                              horizon,
+                              i0,
+                              interv);
+    
+    // Retrieve all results from simulation:
+    // populations:
+    dcDataFrame pop_final = sim.get_world()[popexport].export_dcDataFrame();
+    // epidemic time series
+    dcDataFrame ts = sim.timeseries();
+    dcDataFrame ts_census = sim.get_ts_census_by_SP();
+    
+    // Return R-formatted result:
+    return List::create(Named("population_final") = to_list(pop_final,false),
+                        Named("time_series") = to_list(ts, false),
+                        Named("time_series_census") = to_list(ts_census, false));
 }
 
 
@@ -191,11 +302,11 @@ List naf_run(List params, List simulParams){
 // [[Rcpp::export]]
 List naf_test(List params, List simulParams){
     /// Main test
-	
+    
     vector<unsigned int> res;
-	
+    
     // Model parameters:
-	
+    
     bool debug_mode		= params["debug_mode"];
     
     string dol_distrib	= params["dol_distrib"];
@@ -309,14 +420,14 @@ List naf_test(List params, List simulParams){
                         Named("time_series_census") = to_list(ts_census, false));
 }
 
-	/*
-// [[Rcpp::export]]
-List build_world(List params){
+/*
+ // [[Rcpp::export]]
+ List build_world(List params){
 	
-
+ 
 	vector<ID> id_au = params["id_au"];
 	vector<string> name_au = params["name_au"];
-
+ 
 	ID id_region = 0;
 	string regionName = "RegionOne";
 	vector<areaUnit> auvec = create_area_unit(id_au, name_au, id_region, regionName);
@@ -380,15 +491,15 @@ List build_world(List params){
 	vector<uint> n_school   = params["n_school"];
 	
 	vector<socialPlace> W = build_world(auvec,
-									D_size_hh,      // Households sizes
-									pr_age_hh,      // Age distribution inside households
-									D_size_wrk,
-									D_size_pubt,
-									D_size_school,
-									n_hh,           // number of households
-									n_wrk,
-									n_pubt,
-									n_school);
+ D_size_hh,      // Households sizes
+ pr_age_hh,      // Age distribution inside households
+ D_size_wrk,
+ D_size_pubt,
+ D_size_school,
+ n_hh,           // number of households
+ n_wrk,
+ n_pubt,
+ n_school);
 	
 	//DEBUG
 	cout << "DEBUG: W.size = "<< W.size() << endl;
@@ -396,14 +507,14 @@ List build_world(List params){
 	vector<dcDataFrame> df = export_dcDataFrame(W);
 	
 	Rcpp::List rcpplist;
-
+ 
 	for(unsigned int i=0; i< df.size(); i++)
-		rcpplist.push_back( to_list(df[i], false) );
+ rcpplist.push_back( to_list(df[i], false) );
 	
 	return rcpplist;
-
-}
-	 */
+ 
+ }
+ */
 
 
 
@@ -416,11 +527,11 @@ List naf_test_SEIR_vs_ODE(List params, List simulParams){
     /// This runs a SEIR model implemented
     /// in this agent-based model.
     /// The goal is to compare its output to an ODE model.
-	
+    
     vector<unsigned int> res;
-	
+    
     // Model parameters:
-	
+    
     bool debug_mode		= params["debug_mode"];
     double dol_mean		= params["dol_mean"];
     double doi_mean		= params["doi_mean"];
