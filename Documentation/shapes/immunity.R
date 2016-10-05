ce9f4fd2233b2c203027b1d8e2eb7c17ab735b00
davidchampredon <david.champredon@gmail.com>
2016-10-05 10:08:11 -0400

    minor edits

--- Documentation/shapes/immunity.R
+++ Documentation/shapes/immunity.R
@@ -2,18 +2,16 @@
 # Humoral immunity
 
 q <- 100
-p <- 2
 x <- seq(0,q,length.out = 1e3)
 
-
-plot.hum <- function(x,q,p, add) {
-	y <- 1 * ( (q^p-(x)^p)^(1/p) )/q
+plot.hum <- function(x,q,p,h0, add) {
+	y <- h0 * ( (q^p-(x)^p)^(1/p) )/q
 	if(!add){
 		plot(x,y,typ='l',lwd=6, 
 			 main='Humoral immunity',
 			 ylab='',xlab='age',
 			 ylim = c(0,1),las=1)
-		grid(lty=1)	}
+		grid(lty=2)	}
 	if(add){
 		lines(x,y,typ='l',lwd=2)
 	}
@@ -42,13 +40,14 @@ plot.cel <- function(slope,add) {
 	}
 }
 
-
-pdf('../figures/immunity.pdf',width = 12, height = 6)
+save.plot <- F# TRUE
+if(save.plot) pdf('../figures/immunity.pdf',width = 12, height = 6)
 par(mfrow=c(1,2))
-plot.hum(x,q=100,p=2, add=F)
-plot.hum(x,q=100,p=3, add=T)
+h0 <- 0.5
+plot.hum(x,q=100,p=1.7,h0, add=F)
+plot.hum(x,q=100,p=3.0,  h0, add=T)
 plot.cel(slope=3, add=F)
 plot.cel(slope=1.5, add=T)
-dev.off()
+if(save.plot) dev.off()
 
 
