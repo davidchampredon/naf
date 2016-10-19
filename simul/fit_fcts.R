f49b06d52ab47f70dad814dd8bbae3700b08556a
davidchampredon <david.champredon@gmail.com>
2016-10-19 14:33:09 -0400

    add ABC as a fitting method for age distribution

---
+++ simul/fit_fcts.R
@@ -0,0 +1,308 @@
+####
+####   HELPER FUNCTIONS FOR FIT AGE DISTRIBUTION CONDIITINAL ON HOUSEHOLDS SIZE
+####
+
+# Polynomial regression
+poly.reg <- function(x,y,deg) {
+	df <- data.frame(x,y)
+	L <- lm( y~ poly(x, deg, raw=TRUE), df)
+	return(L$fitted.values)
+}
+
+
+# Error distance b/w 2 curves
+err.dist <- function(x,y,p){
+	n <- min(length(x),length(y))
+	x <- unname(x[1:n])
+	y <- unname(y[1:n])
+	s <- 0
+	for(i in 1:n) s <- s + (x[i]-y[i])^p
+	return(s^(1/p))
+}
+
+
+# Snowfall wrapper:
+run.snow.wrap <- function(seedMC,
+						  prm, 
+						  simul.prm, 
+						  interv.prm, 
+						  world.prm, 
+						  sched.prm,
+						  stoch_build_world){
+	if(stoch_build_world){
+		res <- naf_run(prm, 
+					   simul.prm, 
+					   interv.prm, 
+					   world.prm, 
+					   sched.prm,
+					   seedMC)
+	}
+	else{
+		res <- naf_run_det(prm, 
+						   simul.prm, 
+						   interv.prm, 
+						   world.prm, 
+						   sched.prm,
+						   seedMC)
+	}	
+	return(res)
+}
+
+# Construct the simulation world and 
+# return the simulated _global_ age distribution:
+get.age.update.world <- function(prm, 
+								 simul.prm, 
+								 interv.prm, 
+								 world.prm, 
+								 sched.prm) 
+{
+	simul.prm[['build_world_only']] <- TRUE
+	
+	# Update world parameter
+	# Relevant when age distributions
+	# given household size previously updated:
+	
+	# Remove existing:
+	idx <- which(grepl('pr_age_hh_',names(world.prm)))
+	world.prm[idx] <- NULL
+	# Update with new ones:
+	world.prm   <- c(world.prm,
+					 load.age.hh(base.fname, 
+					 			max(world.prm[['max_hh_size']])) )
+	world.prm <- gen_world_ontario(world.prm)
+	
+	# Note: put in a list format to be consistent
+	# with what snowfall does. 
+	# Here snowfall is not really usefull as we most likely
+	# run with 1 MC (when deterministic built for world).
+	# I had to delete snowfall in this function as ther was 
+	# issues with embedded export of functions...
+	res <- list(run.snow.wrap(1,
+							  prm, 
+							  simul.prm, 
+							  interv.prm, 
+							  world.prm, 
+							  sched.prm,
+							  stoch_build_world))
+	
+	# Average the global age distributions
+	# across all MC iterations:
+	a <- list()
+	for(i in seq_along(res)){
+		a[[i]] <- res[[i]][['ages']]
+	}
+	amx <- max(unlist(lapply(a, max)))+1
+	sim.age <- 0:amx
+	
+	M <- matrix(nrow = length(sim.age)-1, ncol=length(res))
+	for(i in seq_along(res)){
+		h <- hist(a[[i]], breaks = sim.age, plot = F)
+		sim.age.prop.i <- h$counts / sum(h$counts)	
+		M[,i] <- sim.age.prop.i
+	}
+	sim.age.prop <- apply(M,1,FUN = mean)
+	
+	return(list(sim.age = sim.age, 
+				sim.age.prop = sim.age.prop))
+}
+
+
+error.function <- function(agemean.vec,
+						   a.vec,
+						   prm, 
+						   simul.prm, 
+						   interv.prm, 
+						   world.prm, 
+						   sched.prm) {
+	
+	# Generate age (target) distributions given household size
+	# with parameters 'agemean' and 'a':
+	gen.all.ad.hhsz(agemean.vec, a.vec, 
+					path.save='../data/households/')
+	
+	
+	A <- get.age.update.world(prm        = prm, 
+							  simul.prm  = simul.prm, 
+							  interv.prm = interv.prm, 
+							  world.prm  = world.prm, 
+							  sched.prm  = sched.prm) 
+	# Global age distribution:
+	sim.age      <- A[['sim.age']]
+	sim.age.prop <- A[['sim.age.prop']]
+	sim.age2     <- sim.age[1:length(sim.age.prop)]
+	
+	
+	
+	# Retrieve the target global age distribution from data:
+	target.ad <- read.csv('../data/ages/size-distrib-ages.csv')
+	idx       <- which(target.ad$age <= max(sim.age))
+	t.ad      <- target.ad[idx,]
+	t.ad$prop <- t.ad$prop / sum(t.ad$prop)
+	
+	# Polynomial regression
+	# (fit is made on this smooth regression
+	#  rather than noisy simulation outputs & data)
+	deg      <- 7
+	t.ad.reg <- poly.reg(x=t.ad$age, y=t.ad$prop, deg)
+	sim.reg  <- poly.reg(x=sim.age2, y=sim.age.prop, deg)
+	
+	
+	# Error value (to be minimized):
+	err <- err.dist(sim.reg, t.ad.reg, p=2)
+	
+	return(list(err = err,
+				t.ad = t.ad,
+				sim.age = sim.age2,
+				sim.age.prop = sim.age.prop,
+				target.reg = t.ad.reg,
+				sim.reg = sim.reg))
+}
+
+
+eval_f <- function(x,
+				   prm, 
+				   simul.prm, 
+				   interv.prm, 
+				   world.prm, 
+				   sched.prm){
+	agemean.vec <- x[1:6]
+	a.vec       <- x[7:12]
+	EF <- error.function(agemean.vec, 
+						 a.vec,
+						 prm, 
+						 simul.prm, 
+						 interv.prm, 
+						 world.prm, 
+						 sched.prm)
+	return(EF[['err']])
+}
+
+
+# Optimization using 'nlopt' library
+
+optim_nlopt <- function(algo.name, x0, lb, ub,  
+						prm, 
+						simul.prm, 
+						interv.prm, 
+						world.prm, 
+						sched.prm) {
+	
+	# for all options, see: nloptr.print.options()
+	opts <- list('algorithm'   =algo.name, #'NLOPT_GN_ISRES', # NLOPT_GN_DIRECT_L, NLOPT_GN_ISRES,     NLOPT_GN_CRS2_LM', # 'NLOPT_LN_COBYLA',
+				 'print_level' = 3,
+				 'maxtime'     = 60*simul.prm[['fit_maxtime_minutes']])
+	
+	opt.val <- nloptr(x0 = x0, 
+					  eval_f = eval_f, 
+					  lb = lb, 
+					  ub = ub, 
+					  opts = opts,
+					  prm        = prm, 
+					  simul.prm  = simul.prm, 
+					  interv.prm = interv.prm, 
+					  world.prm  = world.prm, 
+					  sched.prm  = sched.prm)
+	return(opt.val$solution)
+}
+
+# Wrap for parallel execution:
+snow.abc <- function(i,
+					 MABC,
+					 prm, 
+					 simul.prm, 
+					 interv.prm, 
+					 world.prm, 
+					 sched.prm){
+	# i=1
+	x <- MABC[i,]
+	return(eval_f(x = x,
+				  prm        = prm, 
+				  simul.prm  = simul.prm, 
+				  interv.prm = interv.prm, 
+				  world.prm  = world.prm, 
+				  sched.prm  = sched.prm) )
+}
+
+optim_abc <- function(n.abc, 
+					  n.cpu,
+					  lb, ub,  
+					  prm, 
+					  simul.prm, 
+					  interv.prm, 
+					  world.prm, 
+					  sched.prm){
+	
+	# Matrix of all parameters combinations:
+	b <- length(lb) ; stopifnot(b==length(ub))
+	MABC <- matrix(nrow=n.abc, ncol=b)
+	for(j in 1:b){
+		tmp <- seq(lb[j],ub[j],length.out = n.abc)
+		MABC[,j] <- sample(tmp)
+	}
+	
+	sfInit(parallel = (n.cpu>1), cpu = n.cpu)
+	# sfExport(list=list('eval_f','error.function','gen.all.ad.hhsz','gen.ad',
+	# 				   'get.age.update.world','load.age.hh'))
+	sfExportAll()
+	sfLibrary(naf, lib.loc = R.library.dir) 
+	res <- sfSapply(x          = 1:n.abc, 
+					fun        = snow.abc,
+					MABC       = MABC,
+					prm        = prm, 
+					simul.prm  = simul.prm, 
+					interv.prm = interv.prm, 
+					world.prm  = world.prm, 
+					sched.prm  = sched.prm,
+					simplify   = FALSE)
+	sfStop()
+	idx.best <- which.min(res)
+	
+	return(list(x.best = MABC[idx.best,], 
+				fx.best = res[idx.best]))
+}
+
+
+plot.best.fit <- function(xbest,
+						  prm, 
+						  simul.prm, 
+						  interv.prm, 
+						  world.prm, 
+						  sched.prm) {
+	
+	EF <- error.function(agemean.vec = xbest[1:6],
+						 a.vec       = xbest[7:12],
+						 prm        = prm, 
+						 simul.prm  = simul.prm, 
+						 interv.prm = interv.prm, 
+						 world.prm  = world.prm, 
+						 sched.prm  = sched.prm)
+	
+	err          <- EF[['err']]
+	t.ad         <- EF[['t.ad']]
+	sim.age      <- EF[['sim.age']]
+	sim.age.prop <- EF[['sim.age.prop']]
+	target.reg   <- EF[['target.reg']]
+	sim.reg      <- EF[['sim.reg']]
+	
+	pdf('plot-fit-hhsz-age.pdf', width = 15, height = 10)
+	par(mfrow=c(1,1))
+	plot(t.ad$age, t.ad$prop, 
+		 ylim= range(t.ad$prop,sim.age.prop,0),
+		 main = paste('err =',round(err,5)),
+		 cex=1.0, typ='p', col='lightgrey')
+	lines(t.ad$age, target.reg, lty =2, lwd =2)
+	grid()
+	lines(sim.age,
+		  sim.age.prop, 
+		  col = 'pink',
+		  pch=16, cex=0.7,  typ='o')
+	lines(sim.age, sim.reg, lty =1, lwd=2, col='red')
+	
+	read.all.ad.hhsz(path = '../data/households/')
+	dev.off()
+}
+
+
+
+
+
--- simul/fit_hhsz_age_distrib.R
+++ simul/fit_hhsz_age_distrib.R
@@ -21,12 +21,12 @@ library(nloptr)
 
 source('analysis_tools.R')
 source('build-world-det-sizegen.R')
+source('fit_fcts.R')
+source('../data/households/gen-ad-hhsz-fcts.R')
 source(paste0(param.model.dir,'read-prm.R'))
 source(paste0(param.model.dir,'read-world-prm.R'))
 source(paste0(param.model.dir,'read-interv.R'))
 source(paste0(param.model.dir,'read-sched.R'))
-
-source('../data/households/gen-ad-hhsz-fcts.R')
 
 # Parameter file names
 fname.prm.epi      <- 'prm-epi.csv'
@@ -42,7 +42,6 @@ interv.prm <- list()
 world.prm  <- list()
 sched.prm  <- list()
 
-
 ### ==== Model Parameters ====
 
 prm[['debug_mode']] <- FALSE
@@ -53,9 +52,6 @@ simul.prm <- read.prm(filename = paste0(param.model.dir,fname.prm.simul),
 					  prm = simul.prm)
 
 stoch_build_world <- simul.prm[['build_world_stoch']]
-
-
-###  ==== World parameters ====
 
 world.prm <- load.world.prm(filename = paste0(param.model.dir,fname.prm.au), 
 							path.prm = param.model.dir)
@@ -74,233 +70,64 @@ base.fname  <- paste0(data.dir,'households/hh_size_ad')
 world.prm   <- c(world.prm,
 				 load.age.hh(base.fname, 
 				 			max.hh.size = max(world.prm[['max_hh_size']])) )
-
 world.prm <- gen_world_ontario(world.prm)
 
 # schedule time slices:
 sched.prm[['timeslice']] <- c(1.0/24, 4.0/24, 4.0/24, 
 							  1.0/24, 2.0/24, 12.0/24)
-
 sched.prm[['sched_desc']]  <- read.schedules(paste0(param.model.dir,fname.schedules), return.names = FALSE)
 sched.prm[['sched_names']] <- read.schedules(paste0(param.model.dir,fname.schedules), return.names = TRUE)
-
-###  ==== Intervention parameters ====
 
 # Baseline, no vax intervention:
 interv.prm <- load.interv.prm(paste0(param.model.dir,fname.prm.interv.0))
 
-# Snowfall wrapper:
-run.snow.wrap <- function(seedMC,
-						  prm, 
-						  simul.prm, 
-						  interv.prm, 
-						  world.prm, 
-						  sched.prm,
-						  stoch_build_world){
-	if(stoch_build_world){
-		res <- naf_run(prm, 
-					   simul.prm, 
-					   interv.prm, 
-					   world.prm, 
-					   sched.prm,
-					   seedMC)
-	}
-	else{
-		res <- naf_run_det(prm, 
-						   simul.prm, 
-						   interv.prm, 
-						   world.prm, 
-						   sched.prm,
-						   seedMC)
-	}	
-	return(res)
-}
-
-
-### ==== Run Simulation ====
-
-error.function <- function(agemean.vec,a.vec) {
-	
-	# Generate age (target) distributions given household size
-	# with parameters 'agemean' and 'a':
-	gen.all.ad.hhsz(agemean.vec, a.vec, 
-					path.save='../data/households/')
-	
-	# Construct the simulation world and 
-	# return the simulated _global_ age distribution:
-	get.age.update.world <- function(prm, 
-									 simul.prm, 
-									 interv.prm, 
-									 world.prm, 
-									 sched.prm) {
-		n.MC  <- simul.prm[['fit_hhsz_age_mc']]
-		n.cpu <- simul.prm[['fit_hhsz_age_cpu']]
-		seeds <- 1:n.MC
-		
-		# Update world parameter
-		# Relevant when age distributions
-		# given household size previously updated:
-		
-		# Remove existing:
-		idx <- which(grepl('pr_age_hh_',names(world.prm)))
-		world.prm[idx] <- NULL
-		# Update with new ones:
-		world.prm   <- c(world.prm,
-						 load.age.hh(base.fname, 
-						 			max(world.prm[['max_hh_size']])) )
-		
-		world.prm <- gen_world_ontario(world.prm)
-		
-		print('==== DEBUG ====')
-		print(world.prm[['pr_age_hh_00_proba']])
-		
-		sfInit(parallel = (n.cpu>1), cpu = n.cpu)
-		sfLibrary(naf,lib.loc = R.library.dir) 
-		
-		simul.prm[['build_world_only']] <- TRUE
-		
-		res <- sfSapply(x          = seeds, 
-						fun        = run.snow.wrap,
-						prm        = prm, 
-						simul.prm  = simul.prm, 
-						interv.prm = interv.prm, 
-						world.prm  = world.prm, 
-						sched.prm  = sched.prm,
-						stoch_build_world = stoch_build_world,
-						simplify   = FALSE)
-		
-		sfStop()
-		
-		# Average the global age distributions
-		# across all MC iterations:
-		a <- list()
-		for(i in seq_along(res)){
-			a[[i]] <- res[[i]][['ages']]
-		}
-		amx <- max(unlist(lapply(a, max)))+1
-		sim.age <- 0:amx
-		
-		M <- matrix(nrow = length(sim.age)-1, ncol=length(res))
-		for(i in seq_along(res)){
-			h <- hist(a[[i]], breaks = sim.age, plot = F)
-			sim.age.prop.i <- h$counts / sum(h$counts)	
-			M[,i] <- sim.age.prop.i
-		}
-		sim.age.prop <- apply(M,1,FUN = mean)
-		
-		return(list(sim.age = sim.age, 
-					sim.age.prop = sim.age.prop))
-	}
-	A <- get.age.update.world(prm, 
-							  simul.prm, 
-							  interv.prm, 
-							  world.prm, 
-							  sched.prm) 
-	# Global age distribution:
-	sim.age      <- A[['sim.age']]
-	sim.age.prop <- A[['sim.age.prop']]
-	sim.age2     <- sim.age[1:length(sim.age.prop)]
-	
-	# Polynomial regression
-	poly.reg <- function(x,y,deg) {
-		df <- data.frame(x,y)
-		L <- lm( y~ poly(x, deg, raw=TRUE), df)
-		return(L$fitted.values)
-	}
-	
-	# Retrieve the target global age distribution from data:
-	target.ad <- read.csv('../data/ages/size-distrib-ages.csv')
-	idx       <- which(target.ad$age <= max(sim.age))
-	t.ad      <- target.ad[idx,]
-	t.ad$prop <- t.ad$prop / sum(t.ad$prop)
-	
-	# Polynomial regression
-	# (fit is made on this smooth regression
-	#  rather than noisy simulation outputs & data)
-	deg      <- 7
-	t.ad.reg <- poly.reg(x=t.ad$age, y=t.ad$prop, deg)
-	sim.reg  <- poly.reg(x=sim.age2, y=sim.age.prop, deg)
-	
-	err.dist <- function(x,y,p){
-		n <- min(length(x),length(y))
-		x <- unname(x[1:n])
-		y <- unname(y[1:n])
-		s <- 0
-		for(i in 1:n) s <- s + (x[i]-y[i])^p
-		return(s^(1/p))
-	}
-	
-	# Error value (to be minimized):
-	err <- err.dist(sim.reg, t.ad.reg, p=2)
-	
-	return(list(err = err,
-				t.ad = t.ad,
-				sim.age = sim.age2,
-				sim.age.prop = sim.age.prop,
-				target.reg = t.ad.reg,
-				sim.reg = sim.reg))
-}
-
 
 ### ==== Constraint optimization ====
 
-eval_f <- function(x){
-	agemean.vec <- x[1:6]
-	a.vec       <- x[7:12]
-	EF <- error.function(agemean.vec,a.vec)
-	return(EF[['err']])
+agemean.vec <- c(65,59,59,35,35,12)   
+a.vec       <- c(1.5,2.5,2.5,1.8,1.8,2.1)
+x0          <- c(agemean.vec, a.vec)
+agemean.hi  <- c(79,79,79,66,66,45)
+agemean.lo  <- c(40,20,5, 20,20,5)
+a.hi        <- 10
+a.lo        <- 0.1
+ub <- c( agemean.hi , rep(a.hi,length(agemean.hi)) )
+lb <- c( agemean.lo , rep(a.lo,length(agemean.lo)) )
+
+optim.method <- 'ABC' # 'ABC' , 'NLOPT'
+
+if(optim.method=='ABC'){
+xbest <- optim_abc(n.abc = simul.prm[['fit_hhsz_age_ABC_n']], 
+				   n.cpu = simul.prm[['fit_hhsz_age_cpu']],
+				   lb, 
+				   ub,  
+				   prm, 
+				   simul.prm, 
+				   interv.prm, 
+				   world.prm, 
+				   sched.prm)
+xbest <- xbest$x.best
 }
 
-agemean.vec <- c(65,59,59,35,35,12)   #c(60,50,50,35,35,10)
-a.vec       <- c(1.5,2.5,2.5,1.8,1.8,2.1)
-
-x0 <- c(agemean.vec, a.vec)
-ub <- c( c(79,79,79,66,66,45) , rep(10,6) )
-lb <- c( c(40,20,5, 20,20,5) ,  rep(0.1,6) )
-
-opts <- list('algorithm'   ='NLOPT_GN_ISRES', # NLOPT_GN_DIRECT_L, NLOPT_GN_ISRES,     NLOPT_GN_CRS2_LM', # 'NLOPT_LN_COBYLA',
-			 'print_level' = 3,
-			 'maxtime'     = 60*simul.prm[['fit_maxtime_minutes']])
-# nloptr.print.options()
-
-opt.val <- nloptr(x0 = x0, 
-				  eval_f = eval_f, 
-				  lb = lb, ub=ub, 
-				  opts = opts)
-
+if(optim.method=='NLOPT'){
+xbest <- optim_nlopt(algo.name = 'NLOPT_GN_ISRES', 
+					 x0, lb, ub,  
+					 prm, 
+					 simul.prm, 
+					 interv.prm, 
+					 world.prm, 
+					 sched.prm)
+}
 save.image(file='fit_hhsz_age.RData')
 
 # Retrieve the optimal value
 # and plot the 'best' distribution:
-xx <- opt.val$solution
-EF <- error.function(agemean.vec = xx[1:6],
-					 a.vec = xx[7:12])
-
-err <- EF[['err']]
-
-t.ad         <- EF[['t.ad']]
-sim.age      <- EF[['sim.age']]
-sim.age.prop <- EF[['sim.age.prop']]
-target.reg   <- EF[['target.reg']]
-sim.reg      <- EF[['sim.reg']]
-
-pdf('plot-fit-hhsz-age.pdf', width = 15, height = 10)
-par(mfrow=c(1,1))
-plot(t.ad$age, t.ad$prop, 
-	 ylim= range(t.ad$prop,sim.age.prop,0),
-	 main = paste('err =',round(err,5)),
-	 cex=1.0, typ='p', col='lightgrey')
-lines(t.ad$age, target.reg, lty =2, lwd =2)
-grid()
-lines(sim.age,
-	  sim.age.prop, 
-	  col = 'pink',
-	  pch=16, cex=0.7,  typ='o')
-lines(sim.age, sim.reg, lty =1, lwd=2, col='red')
-
-read.all.ad.hhsz(path = '../data/households/')
-dev.off()
-
+plot.best.fit(xbest,
+			  prm        = prm, 
+			  simul.prm  = simul.prm, 
+			  interv.prm = interv.prm, 
+			  world.prm  = world.prm, 
+			  sched.prm  = sched.prm)
 
 t1 <- as.numeric(Sys.time())
 message(paste("Computing time:",round((t1-t0)/60,1),"min"))
