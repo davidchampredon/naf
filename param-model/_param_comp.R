1529844c32e59640d65be0a0ffec021a6d6657b7
davidchampredon <david.champredon@gmail.com>
2016-12-16 11:00:11 -0500

    add more studies for parameter comparison

--- param-model/_param_comp.R
+++ param-model/_param_comp.R
@@ -6,17 +6,24 @@ n <- ncol(x)
 dat <- gather(x,'param','v',2:n)
 tmp <- gsub(x = dat$param, pattern = '_lo',replacement = '')
 tmp <- gsub(x = tmp, pattern = '_hi',replacement = '')
-tmp
 
 dat$p <- tmp
 dat$val <- as.numeric(dat$v)
 
 dat.plot <- subset(dat, !is.na(val))
-g <- ggplot(dat.plot)+geom_boxplot(aes(x=paper,y=val)) + facet_wrap(~p, scales = 'free')
+pdf('_param_comp.pdf', width = 15)
+g <- ggplot(dat.plot) + geom_point(aes(x=paper,y=val, colour=paper)) 
+g <- g + facet_wrap(~p, scales = 'free')
 g <- g + xlab('') + ylab('')
-g <- g + theme(axis.text.x = element_text(angle = 20, hjust = 1))
+g <- g + theme(axis.text.x = element_text(angle = 25, hjust = 1))
 plot(g)
 
+
+g <- ggplot(dat.plot) + geom_histogram(aes(x=val), bins=10)+ facet_wrap(~p,scales='free_x')
+g <- g + xlab('parameter value') 
+plot(g)
+
+dev.off()
 
 # dat <- gather(x,'param','v',4:n)
 # dat$val.lo <- dat$v
