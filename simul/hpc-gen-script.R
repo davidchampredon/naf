afa9734a1253181ac47534165b22bf7c64b0307b
davidchampredon <david.champredon@gmail.com>
2017-01-10 15:57:04 -0500

    improve HPC scripts

--- simul/hpc-gen-script.R
+++ simul/hpc-gen-script.R
@@ -23,6 +23,8 @@ for(i in 1:ns){
 	x[2] <- paste0('#PBS -l walltime=',wall.time)
 	x[3] <- '#PBS -l nodes=1:ppn=1
 #PBS -r n
+#PBS -m b
+#PBS -M david.champredon@gmail.com
 
 module load intel64/15.3.187 xz/5.2.2 gcc/5.2.0
 module load gcc bioinformatics/R/3.2.5 bioinformatics/Bioconductor/3.2
