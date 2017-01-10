e640d9967c2f9f4ec9caf35d6c51f3e6a7b280ee
davidchampredon <david.champredon@gmail.com>
2017-01-10 16:03:03 -0500

    update scenario builder

--- simul/scenario-builder.R
+++ simul/scenario-builder.R
@@ -10,9 +10,9 @@
 
 M <- list()
 
-M[['interv_start']]        <- c(-6,0)  #c(-56,-28,0,28)
-M[['interv_cvg_rate']]     <- c(0.0010, 0.0120) # c(0.0010, 0.0030, 0.0060,0.0120)  #c(0.0010, 0.0030, 0.0100)
-M[['interv_target']]       <- 'susceptible' #c('susceptible','young_old')
+M[['interv_start']]        <- c(-56,-28,0,28)
+M[['interv_cvg_rate']]     <- c(0.0010, 0.0030, 0.0060,0.0120)  #c(0.0010, 0.0030, 0.0100)
+M[['interv_target']]       <- c('susceptible','young_old')
 M[['interv_cvg_max_prop']] <- 0.40 #c(0.3, 0.8)
 M[['interv_efficacy']]     <- 0.80 #c(0.2, 0.8)
 M[['contact_rate_mean']]   <- c(3.0)   # c(1.8, 3.6)
