d0eb96a82eb254e97cba793bad009d8bce50bfe5
davidchampredon <david.champredon@gmail.com>
2017-01-12 11:05:03 -0500

    changes in proba hosp and death fits

--- data/frailty/calc-avg-proba-hosp.R
+++
@@ -1,6 +0,0 @@
-age <- read.csv('../ages/size-distrib-ages.csv')
-frail <- read.csv('frailty-canada.csv')
-
-age.select <- subset(age, age%in%frail$age)
-
-avg.proba.hosp.div.alpha <- sum(frail*age.select) / sum(age.select)
--- fit/fit-proba-hosp-death.R
+++ fit/fit-proba-hosp-death.R
@@ -1,22 +1,23 @@
-###
-###   * * * WARNING * * * THIS DOES NOT FIT! IT SIMPLY DISPLAYS DATA 
-###   THE VALUES OF THE PARAMETERS 'proba_hosp' AND 'proba_death_xxx'
-###   ARE SET MANUALLY, SUCH THAT THE RESULTING PROBA FROM THE SIMULATION
-###   MATCH THE DATA.
-###
+age <- read.csv('../data/ages/size-distrib-ages.csv')
+frail <- read.csv('../data/frailty/frailty-canada.csv')
+
+age.select <- subset(age, age%in%frail$age)
+
+frail.wavg <- sum(frail$frailty * age.select$prop) / sum(age.select$prop)
 
 
-library(ggplot2)
-x <- read.csv('../data/frailty/ontario/ontario-influenza-hosp-death.csv')
+TARGET.proba.hosp <- 75 / 1e5  # per 100,000
 
-# reorder propoerly:
-unique(x$age_group)
-as.character(x$age_group)
-x$age_group2 <- factor(x$age_group, levels=c('<1 ','1_4 ','5 _ 14 ','15 _ 24 ','25 _ 44 ','45 _ 64 ','65+ '))
+alpha.hosp.fit <- TARGET.proba.hosp / frail.wavg
 
-pdf('proba-hosp-death-ontario.pdf', width = 10)
-g <- ggplot(x) + geom_boxplot(aes(x=age_group2, y=rate_100000)) + facet_wrap(~type, scales = 'free')
-g <- g + xlab('Age Group') + ylab('per 100,000 individuals per year') 
-g <- g + ggtitle("Death and hospitalization rate for Ontario 2013-2016\n (source: Public Health Ontario)")
-plot(g)
-dev.off()
+TARGET.proba.death.cond.hosp <- 0.30
+alpha.death.fit <- TARGET.proba.death.cond.hosp / frail.wavg
+
+
+print('Suggested alpha values for a fit to:')
+print(paste('Target proba hospitalization: ',TARGET.proba.hosp))
+print(paste('Target proba death cond. on hosp: ',TARGET.proba.death.cond.hosp))
+print('\n')
+print(paste('--> alpha_hosp  =',alpha.hosp.fit))
+print(paste('--> alpha_death =',alpha.death.fit))
+
