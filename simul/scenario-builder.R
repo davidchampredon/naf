69e7af79199a6c759b0f8318e66f8b805cbe09cf
davidchampredon <david.champredon@gmail.com>
2017-02-06 12:14:13 -0500

    adjust contact rate value following last changes
    before: CR=3.0 ==> R0~1.8
    now    : CR=2.5 ==> R0~1.8

--- simul/scenario-builder.R
+++ simul/scenario-builder.R
@@ -15,7 +15,7 @@ M[['interv_cvg_rate']]     <- c(0.0010, 0.0030, 0.0060,0.0120)  #c(0.0010, 0.003
 M[['interv_target']]       <- c('young_old')  #c('never_sympt','young_old')
 M[['interv_cvg_max_prop']] <- 0.40 #c(0.3, 0.8)
 M[['interv_efficacy']]     <- c(0.3, 0.6, 0.9)
-M[['contact_rate_mean']]   <- c(1.7, 3.0)   # c(1.8, 3.6)
+M[['contact_rate_mean']]   <- c(1.7, 2.5)   # c(1.8, 3.6)
 M[['imm_hum_baseline']]    <- 0.00001   
 
 x <- as.data.frame(expand.grid(M))
