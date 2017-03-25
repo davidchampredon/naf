83f75f88ae75bb130a8c57154964d25b351ea62a
davidchampredon <david.champredon@gmail.com>
2017-03-24 21:41:10 -0400

    update scenario generator for sensi analysis

--- simul/scenario-builder-sensana.R
+++ simul/scenario-builder-sensana.R
@@ -9,8 +9,12 @@
 # list 'M': Combination of all parameters
 M <- list()
 M[['interv_start']]        <- c(-28,14)
-M[['interv_cvg_rate']]     <- c(0.003)   #c(0.0030, 0.0120)  
-# M[['interv_efficacy']]     <- c(0.4, 0.9)
+M[['interv_cvg_rate']]     <- c(0.0030, 0.0120)  
+M[['interv_target']]       <- c('priority_age5_frailty')
+M[['interv_cvg_max_prop']] <- 0.40 
+M[['interv_efficacy']]     <- c(0.4, 0.9)
+M[['imm_hum_baseline']]    <- 0.00001  
+
 x <- as.data.frame(expand.grid(M))
 
 # list 'A': Change one value at the time (NO combination!) 
@@ -19,13 +23,12 @@ x <- as.data.frame(expand.grid(M))
 # baseline value must be the first one for each vector!
 #
 A <- list()
-A[['imm_hum_baseline']]    <- c(0.001, 0.21)
-A[['contactAssort_lambda']]<- c(0.1, 0.22) #c(0.1, 0.05, 0.2)
-# A[['frailty_sd']]          <- c(0.1, 0.02, 0.5)
-# A[['imm_cell_max']]        <- c(0.45, 0.2, 0.9)
-# A[['contact_rate_CV']]     <- c(0.75, 0.3, 1.5)
-# A[['proba_move']]          <- c(0.98, 0.8)
-# A[['asymptom_infectiousness_ratio']]<- c(0.1, 0.05, 0.2)
+A[['contactAssort_lambda']]<- c(0.1, 0.05, 0.2)
+A[['frailty_sd']]          <- c(0.1, 0.02, 0.2)
+A[['imm_cell_max']]        <- c(0.45, 0.1, 0.9)
+A[['contact_rate_CV']]     <- c(0.75, 0.3, 1.5)
+A[['proba_move']]          <- c(0.98, 0.8)
+A[['asymptom_infectiousness_ratio']]<- c(0.1, 0.05, 0.4)
 
 # # pubT prop
 # proba_change_sp_other 
